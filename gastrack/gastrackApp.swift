f0c05e90f3e7c3c221a0291a08da7bbdfea62b90
Nate Schaffner <nathanial.schaffner@gmail.com>
2022-06-14 00:02:13 -0400

    Initial Commit

---
+++ gastrack/gastrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  gastrackApp.swift
+//  gastrack
+//
+//  Created by Nate Schaffner on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct gastrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
